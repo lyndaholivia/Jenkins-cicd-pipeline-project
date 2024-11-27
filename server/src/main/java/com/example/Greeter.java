2acad4225a8b763839a333463a0d7f4567edd073
Lynda Nwabudike <lynda.holivia@gmail.com>
2024-11-27 23:35:20 +0000

    commit2

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -17,5 +17,6 @@ public class Greeter {
    */
   public final String greet(final String someone) {
     return String.format("Hello Avinash, %s!", someone);
+    String DB_PASSWORD = “Admin@1234”; 
   }
 }
